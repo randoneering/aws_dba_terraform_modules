58fc3253e973c02459570cc656a9d97a4a98d20d
randonering <justin@randoneering.tech>
2024-06-04 21:29:41 -0500

    add docdb module

---
+++ docdb/outputs.tf
@@ -0,0 +1,8 @@
+output "cluster_endpoint" {
+    value = aws_docdb_cluster.docdb.endpoint
+}
+
+output "cluster_arn" {
+    value = aws_docdb_cluster.docdb.arn
+}
+
---
+++ docdb/vars.tf
@@ -0,0 +1,31 @@
+variable "app_name_instance" {
+  type        = string
+  description = "The name of the application this database is being created for"
+}
+
+variable "instance_class" {
+  type    = string
+  default = "db.r6g.xlarge"
+}
+
+# Straightforward-variable for number of instances to create. Default is 1
+variable "number_of_instances" {
+  type    = number
+  default = 1
+}
+
+# Creation of parameter group happens in root/application folder. 
+variable "parameter_group" {
+  type = string
+}
+
+# Creation of kms_key_arn can either happen in seperate module or in root/application folder.
+variable "kms_key_arn" {
+  type = string
+}
+
+variable "deletion_protection" {
+  type = string
+  default = true
+  
+}
